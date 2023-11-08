aa4ca0d8dd1405a9be576fc98f67efe434fe38e8
PedroBovo <131620673+PedroBovo@users.noreply.github.com>
2023-11-08 14:59:28 +0000

    Crianção do endPoint /oi

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World, sou pedro')
+})
+app.get('/oi', function(req, res) {
+  res.send('ola, mundo')
 })
 
 app.listen(3000)
