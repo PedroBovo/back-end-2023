85571c84b19dd9d8054dde1187097319a7b80b90
PedroBovo <131620673+PedroBovo@users.noreply.github.com>
2023-11-09 14:11:48 +0000

    Criação do endpoint Read by ID - [Get] /item/:id

--- index.js
+++ index.js
@@ -7,8 +7,29 @@ app.get('/', function (req, res) {
 app.get('/oi', function(req, res) {
   res.send('ola, mundo')
 })
+
 const lista = ["Rick Sanchez", "Morty Smith", "Summer Smith"]
+//                0               1               2
+
+
+// Read all - [GET] /item/
 app.get('/item', function (req, res){
         res.send(lista)
 })
+
+// Read by ID - [Get] /item/:id
+app.get('/item/:id', function(req, res){
+
+  // pegamos o id de rota e subtraimos -1 para ficar equivalente ao indice da lista que comeca em 0
+  const id = req.params.id - 1
+
+//acessamos o item na lista, usando o indice corrigido
+  const item = lista[id]
+  
+//enviamos o item como resposta do endpoint
+  res.send(item)
+})
+
+
+
 app.listen(3000)
